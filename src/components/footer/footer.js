dc8b662a357e6cefca06e505b3ecfaf980eca8d4
shridhar-t <thakur.shridhar3093@gmail.com>
2020-05-28 01:12:34 +0530

    Change footer,navbar,home,image

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,20 +9,20 @@ const footer=(props)=>{
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <div className="title">MADE BY</div>
-                        <h4>IIT Madras Interns</h4>
+                        <div className="title"><h4>Made By</h4></div>
+                        <h5>IIT Madras Interns</h5>
                     </div>
                     <div className="col">
-                    <div className="title">USEFUL LINKS</div>
+                    <div className="title"><h4>Links</h4></div>
                         <ul className="list-unstyled">
-                        <li className="list"><Link to="/" type="button" className="text">Home</Link></li>
-                        <li className="list"><Link to="/get-started" className="text">Get started</Link></li>
-                        <li className="list"><Link to="/about-us" className="text">About-Us</Link></li>
+                        <li className="list"><Link to="/" type="button" ><h5 className="text">Home</h5></Link></li>
+                        <li className="list"><Link to="/get-started" ><h5 className="text">Get started</h5></Link></li>
+                        <li className="list"><Link to="/about-us"><h5 className="text">About Us</h5></Link></li>
                         &copy;{new Date().getFullYear()} IIT Madras | All rights reserved
                         </ul>
                     </div>
                     <div className="col">
-                    <div className="title">Follow Us On</div>
+                    <div className="title"><h4>Follow Us On</h4></div>
                     <ul className="list-unstyled">
                     <li className="list"><a href="https://www.facebook.com/" className="text">
                     <img  src={facebook} alt="facebook" height="30px"/>
--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -1,17 +1,15 @@
 import {Link } from 'react-router-dom';
 import React from 'react';
 import './navBar.css';
-import homeicon from'./browser.png'
 function navBar() {
     return (
         <div className="total">
         <div className="nav">
-        <Link to="/" type="img" src={homeicon} class="homepage"><img src={homeicon} alt="Home" height="30px"></img></Link>
-        <Link to="/get-started" className="text nv">Get started</Link>
-
-        </div >
+        <Link to="/" class="text nv">HPC</Link>
+        </div>
         <div className="nav right">
-        <Link to="/about-us" className="text nv">About-Us</Link>
+        <Link to="/get-started" className="text nv">Get started</Link>
+        <Link to="/about-us" className="text nv">About Us</Link>
         </div>
         </div>
     )
