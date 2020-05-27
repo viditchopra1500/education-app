a930b1726c5666b1aa489822282fd5a0b6881aea
viditchopra1500 <vidit1500@gmail.com>
2020-05-28 01:24:30 +0530

    color matching

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -1,16 +1,15 @@
 import {Link } from 'react-router-dom';
 import React from 'react';
 import './navBar.css';
-import homeicon from'./browser.png'
 function navBar() {
     return (
         <div className="total">
         <div className="nav">
-        <Link to="/" type="img" src={homeicon} class="homepage"><img src={homeicon} alt="Home" height="30px"></img></Link>
-        <Link to="/get-started" className="text nv">Get started</Link>
+        <Link to="/" ><h4 className="text">Education App</h4></Link>
 
         </div >
         <div className="nav right">
+        <Link to="/get-started" className="text nv">Get started</Link>
         <Link to="/about-us" className="text nv">About-Us</Link>
         </div>
         </div>
