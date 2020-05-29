f3e13b82a106ca645b09aa7e80bb153ef5c9811e
viditchopra1500 <vidit1500@gmail.com>
2020-05-29 18:56:51 +0530

    updated stuff

--- src/components/getStarted/getStarted.js
+++ src/components/getStarted/getStarted.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import ReactPlayer from 'react-player'
+import './getStarted.css'
 const getStarted=(props)=>{
     return (
-        <div className="video">
-            <ReactPlayer url='https://www.youtube.com/watch?v=JGwWNGJdvx8'
-             playing 
-             controls="true"
-             width="1200px"
-             height="670px"/>  
-        </div>
+        <section className="started">
+            <div className="material container-fluid">
+                    <div className="video-player">
+                     <ReactPlayer url='https://www.youtube.com/watch?v=JGwWNGJdv8'
+                    playing 
+                    controls="true"
+                    width="50%"
+                    height="50%"/>
+                    </div> 
+            <div class="panel panel-primary" id="result_panel">
+                <div class="panel-heading"><div >Topics</div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                            <li class="list-group-item"><strong>Signature
+                                Accommodations</strong>(1480m)
+                            </li>
+                        </ul>
+                     </div>
+                    </div> 
+                </div>
+            </div>
+        </section>
     )
 };
 
