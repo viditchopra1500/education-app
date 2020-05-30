02dc596b06180aff7aa16a64721949bd9ada029e
viditchopra1500 <vidit1500@gmail.com>
2020-05-31 02:36:16 +0530

    fixed navbar

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './navBar.css';
 function navBar() {
     return (
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
 <Link class="nav-link brand-name" to="/">HPC</Link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@ function navBar() {
   <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       </ul>
     <form class="form-inline my-2 my-lg-0">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <ul class="navbar-nav mr-auto my-2 mt-lg-0">
     <Link class="nav-link mr-sm-2" to="/get-started">Get started</Link>
     <Link class="nav-link mr-sm-2" to="/about-us">About Us</Link>
       </ul>
